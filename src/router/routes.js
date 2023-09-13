421e0408e2f0caaaa0925d6379bc5a157f422c5c
Guilherme Julio <guilhermejuliocode@gmail.com>
2023-09-13 13:00:43 -0300

    Config routes

---
+++ src/router/routes.js
@@ -0,0 +1,46 @@
+const routes = [
+  {
+    path: "/",
+    component: () => import("layouts/LoginLayout.vue"),
+    children: [
+      {
+        path: "",
+        name: "loginDefault",
+        component: () => import("pages/Login.vue")
+      },
+      {
+        path: "login",
+        name: "login",
+        component: () => import("pages/Login.vue")
+      },
+      {
+        path: "register",
+        name: "register",
+        component: () => import("pages/Register.vue")
+      }
+    ]
+  },
+  {
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      {
+        path: "home",
+        name: "home",
+        component: () => import("pages/IndexPage.vue")
+      }
+    ],
+    meta: {
+      requiresAuth: true
+    }
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue")
+  }
+];
+
+export default routes;
