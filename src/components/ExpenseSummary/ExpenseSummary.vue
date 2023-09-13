066598684999a855d10a4039cadec545573a6b23
Guilherme Julio <guilhermejuliocode@gmail.com>
2023-09-13 12:59:55 -0300

    Expense summary

---
+++ src/components/ExpenseSummary/ExpenseSummary.vue
@@ -0,0 +1,73 @@
+<template>
+  <SummaryContainer>
+    <div class="summary-card highlight-background">
+      <header>
+        <p>Despesas cadastradas</p>
+        <q-icon name="check_circle" size="1.5em" />
+      </header>
+      <strong>{{ acceptedLeads }}</strong>
+    </div>
+
+    <div class="summary-card total-expenses">
+      <header>
+        <p>Valor total de despesas</p>
+        <q-icon name="arrow_upward" size="1.5em" />
+      </header>
+      <strong>R${{ inviteLeads }}</strong>
+    </div>
+  </SummaryContainer>
+</template>
+
+<script>
+import { ref } from "vue";
+import { defineComponent } from "vue";
+import SummaryContainer from "./SummaryContainer.vue";
+
+export default defineComponent({
+  name: "ExpenseSummary",
+  components: {
+    SummaryContainer,
+  },
+  setup() {
+    const inviteLeads = 10;
+    const rejectLeads = 5;
+    const acceptedLeads = 15;
+
+    return {
+      inviteLeads,
+      rejectLeads,
+      acceptedLeads,
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.summary-card {
+  background: var(--q-color-primary);
+  padding: 1rem 2rem;
+  border-radius: 0.25rem;
+  color: var(--q-color-dark);
+}
+
+.summary-card > header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.highlight-background {
+  background: #046f3d;
+  color: #fff;
+}
+.total-expenses {
+  background: #b92e34;
+  color: #fff;
+}
+strong {
+  display: block;
+  margin-top: 1rem;
+  font-size: 2rem;
+  font-weight: 600;
+  line-height: 3rem;
+}
+</style>
---
+++ src/components/ExpenseSummary/SummaryContainer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="summary-container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "SummaryContainer",
+  setup(props) {
+    const highlight = ref(props.highlight || false);
+
+    return {
+      highlight,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.summary-container {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 2rem;
+}
+</style>
